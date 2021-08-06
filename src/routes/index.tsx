54a6c65a611fe3ba964df6df59b1a1eacbdfae96
JonatasAlves9 <alvesjonatas99@gmail.com>
2021-08-06 20:28:08 -0300

    feat: stack navigation in app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { TopTabs } from './topTabs.routes';
+import { StackRoutes } from './stack.routes';
 export function Routes() {
   return (
     <NavigationContainer>
-      <TopTabs />
+      <StackRoutes />
     </NavigationContainer>
   );
 }
---
+++ src/routes/stack.routes.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { TopTabs } from './topTabs.routes';
+
+const Stack = createNativeStackNavigator();
+
+export function StackRoutes() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Home" component={TopTabs} />
+    </Stack.Navigator>
+  );
+}
